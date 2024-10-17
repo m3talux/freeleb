4d5bec2df10253752f042551bf4d81da80d98c23
Alex <alexandre.elkhoury@kardinal.ai>
2024-10-18 01:24:00 +0200

    Fix fill opacity

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -109,6 +109,7 @@ const MapComponent: React.FC = () => {
                                         strokeColor: 'rgba(159,159,159)',
                                         strokeOpacity: 1,
                                         strokeWeight: 2,
+                                        fillOpacity: 0.8,
                                     }}
                                     onMouseOver={() => setHoveredPolygon({
                                         name: feature.properties.NAME_2,
